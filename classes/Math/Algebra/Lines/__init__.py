715e860f85348fbb9b450f44da3570015ad88c7b
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-11 11:29:48 -0400

    deployed

--- classes/Math/Algebra/Lines/__init__.py
+++ classes/Math/Algebra/Lines/__init__.py
@@ -1,5 +1,4 @@
-import Line
-import Arc
-import Curve
-import Straight
-import Segment
+import Classes.Math.Algebra.Lines.Arc as Arc
+import Classes.Math.Algebra.Lines.Curve as Curve
+import Classes.Math.Algebra.Lines.Straight as Straight
+import Classes.Math.Algebra.Lines.Segment.Segment as Segment
