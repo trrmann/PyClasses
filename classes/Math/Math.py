a76d797470cc08f791300a9f516685b1e4d7c4db
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-15 21:14:46 -0400

    deployed

--- classes/Math/Math.py
+++ classes/Math/Math.py
@@ -1,12 +1,9 @@
 # Math.py
 import Classes.Classes as Classes
 
-class Math(Classes):
-    def __new__(cls):
+class Math(Classes.Classes):
+    def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
-
-#    def __new__(cls, *args, **kwargs):
-#        return super().__new__(cls)
 
     def __init__(self, className = "Math"):
         super().__init__(self, className)
