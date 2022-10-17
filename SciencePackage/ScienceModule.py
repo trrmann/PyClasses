c74264ee9ab075288cbd6c32b6b52b6accbe51b5
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-17 00:56:07 -0400

    deployed

--- SciencePackage/ScienceModule.py
+++ SciencePackage/ScienceModule.py
@@ -6,7 +6,7 @@ class Science(Classes):
         return super().__new__(cls)
 
     def __init__(self, className = "Science"):
-        super().__init__(self, className)
+        super().__init__(className)
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}(className={self.className})"
