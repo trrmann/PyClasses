aa047fecef2214ec1ff16d12e4050f2bafe465b8
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-17 13:24:06 -0400

    deployed

--- MathPackage/MathErrorModule.py
+++ MathPackage/MathErrorModule.py
@@ -1,13 +1,13 @@
 # MathError.py
 from ClassesPackage.ClassesErrorModule import ClassesError
+from MathPackage.MathModule import Math
 
 class MathError(ClassesError):
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
 
-    def __init__(self):
-        pass
-
+    def __init__(self, msg: str, classes: Math=Math()):
+        super().__init__(str(msg), classes)
+ 
     def __repr__(self) -> str:
-        return f"{type(self).__name__}"
-
+        return f"{type(self).__name__}(classes:({self.classes}), msg=\"{self.msg}\")"
