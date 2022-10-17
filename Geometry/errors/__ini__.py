c6d31b7fe101a8ade63da7f0c75fea122a734e00
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-16 20:00:25 -0400

    deployed

--- Geometry/errors/__ini__.py
+++
@@ -1,3 +0,0 @@
-import Classes.Math.Geometry.errors.MissingDimensionError as MissingDimensionError
-import Classes.Math.Geometry.errors.UndefinedShapeError as UndefinedShapeError
-import Classes.Math.Geometry.errors.UndefinedOrientationError as UndefinedOrientationError
