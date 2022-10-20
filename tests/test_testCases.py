d73ad2eeaf793c28470db8f4ccb868fb7a522da1
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-20 16:06:49 -0400

    deployed

--- tests/test_testCases.py
+++ tests/test_testCases.py
@@ -9,10 +9,10 @@ from ClassesPackage.TestCaseModule import TestCase
 functionDictionary = {"Classes": Classes,
                       "print": print,
                       "float": float}
-testCase1 = TestCase("case 1", "Classes", {})
-testCase2 = TestCase("case 2", "print", {"Test"}, expected_std_output="Test")
-testCase3 = TestCase("case 3", "float", "1.5", expected_output=1.5)
-testCase4 = TestCase("case 4", "float", "failMe", expected_exception=ValueError)
+testCase1 = TestCase("case 1", "Classes", arguments={})
+testCase2 = TestCase("case 2", "print", arguments={"Test"}, expected_std_output="Test")
+testCase3 = TestCase("case 3", "float", arguments="1.5", expected_output=1.5)
+testCase4 = TestCase("case 4", "float", arguments="failMe", expected_exception=ValueError)
 testCasesDictionary = {testCase1.testCaseName: testCase1,
                         testCase2.testCaseName: testCase2,
                         testCase3.testCaseName: testCase3,
