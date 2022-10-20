d046194cf1a6240912db46707dcdc60abf3ae697
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-20 16:05:36 -0400

    deployed

--- tests/test_testCases.py
+++ tests/test_testCases.py
@@ -11,8 +11,8 @@ functionDictionary = {"Classes": Classes,
                       "float": float}
 testCase1 = TestCase("case 1", "Classes", {})
 testCase2 = TestCase("case 2", "print", {"Test"}, expected_std_output="Test")
-testCase3 = TestCase("case 3", "float", {"1.5"}, expected_output=1.5)
-testCase4 = TestCase("case 4", "float", {"failMe"}, expected_exception=ValueError)
+testCase3 = TestCase("case 3", "float", "1.5", expected_output=1.5)
+testCase4 = TestCase("case 4", "float", "failMe", expected_exception=ValueError)
 testCasesDictionary = {testCase1.testCaseName: testCase1,
                         testCase2.testCaseName: testCase2,
                         testCase3.testCaseName: testCase3,
