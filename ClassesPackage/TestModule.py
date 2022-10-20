d934ee0beda0a542b47f848e25aefb62b5cc96c2
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-20 14:19:29 -0400

    deployed

--- ClassesPackage/TestModule.py
+++ ClassesPackage/TestModule.py
@@ -19,7 +19,7 @@ class Test(Classes):
         full_results = {}
         full_results[self.result_test_result_key] = False
         for key, case in self.testCases.items():
-            full_results[key] = case.execute(self.functionDictionary)
+            full_results[key] = case.execute(functionDictionary = self.functionDictionary)
             full_results[key][self.result_eval_results_key] = case.eval_results(full_results[key])
             full_results[self.result_test_result_key] = full_results[self.result_test_result_key] and not full_results[key][self.result_eval_results_key]
         full_results[self.result_test_result_key] = not full_results[self.result_test_result_key]
