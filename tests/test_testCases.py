1e9248ba7cb03f9fc9ca3393776dc14d5e71b966
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-20 11:56:52 -0400

    deployed

--- tests/test_testCases.py
+++ tests/test_testCases.py
@@ -1,10 +1,12 @@
 # test_testCases.py
 
-from ClassesPackage.ClassesModule import Classes
+import ClassesPackage.ClassesModule as ClassesModule
+#from ClassesPackage.ClassesModule import Classes
+
 from ClassesPackage.TestModule import Test
 from ClassesPackage.TestCaseModule import TestCase
 
-functionDictionary = {"Classes":Classes}
+functionDictionary = {"Classes": ClassesModule.Classes}
 testCase1 = TestCase("case 1", "Classes", {}, stdin_input = "", expected_output = "", expected_std_output = "", expected_exception = Exception())
 testCasesDictionary = {testCase1.testCaseName: testCase1}
 
