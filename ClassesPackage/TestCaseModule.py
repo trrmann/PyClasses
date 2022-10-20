883271a8fd581cd3ab83e354e6d07d11210c9c39
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-20 15:43:07 -0400

    deployed

--- ClassesPackage/TestCaseModule.py
+++ ClassesPackage/TestCaseModule.py
@@ -73,9 +73,12 @@ class TestCase(Classes):
         print(self)
         print(resultsDictionary)
         result = True
-        if self.expected_output != None: result = result and (resultsDictionary[self.result_output_key] == self.expected_output)
-        if self.expected_std_output != None: result = result and (resultsDictionary[self.result_std_output_key] == self.expected_std_output)
-        if self.expected_exception != None: result = result and (resultsDictionary[self.result_exception_key] == self.expected_exception)
+        if self.expected_output != None:
+            result = result and (resultsDictionary[self.result_output_key] == self.expected_output)
+        if self.expected_std_output != None:
+            result = result and (resultsDictionary[self.result_std_output_key] == self.expected_std_output)
+        if self.expected_exception != None:
+            result = result and (resultsDictionary[self.result_exception_key] == self.expected_exception)
         return result
 
     def setup_method(self, stdin_input):
