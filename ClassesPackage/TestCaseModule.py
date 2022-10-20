2d33cdb07c6cf3af1101417cd2a2b8bbf4746b78
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-20 15:38:33 -0400

    deployed

--- ClassesPackage/TestCaseModule.py
+++ ClassesPackage/TestCaseModule.py
@@ -71,10 +71,11 @@ class TestCase(Classes):
     def eval_results(self, **kwargs):
         resultsDictionary = kwargs["resultsDictionary"]
         print(self)
+        print(resultsDictionary)
         result = True
-        if self.expected_output != None: result = result and not(resultsDictionary[self.result_output_key] == self.expected_output)
-        if self.expected_std_output != None: result = result and not(resultsDictionary[self.result_std_output_key] == self.expected_std_output)
-        if self.expected_exception != None: result = result and not(resultsDictionary[self.result_exception_key] == self.expected_exception)
+        if self.expected_output != None: result = result and (resultsDictionary[self.result_output_key] == self.expected_output)
+        if self.expected_std_output != None: result = result and (resultsDictionary[self.result_std_output_key] == self.expected_std_output)
+        if self.expected_exception != None: result = result and (resultsDictionary[self.result_exception_key] == self.expected_exception)
         return result
 
     def setup_method(self, stdin_input):
