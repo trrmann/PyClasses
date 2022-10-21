89d8c49b6f9cc405df949f4e49f9dcbf5728a837
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-21 15:57:13 -0400

    deployed

--- ClassesPackage/TestCaseModule.py
+++ ClassesPackage/TestCaseModule.py
@@ -146,7 +146,7 @@ class TestCase(Classes):
             elif (self.functionKeyWordArguments == None):
                 print(self.functionName)
                 print(self.functionArguments)
-                resultsDictionary[self.result_output_key] = functionDictionary.get(self.functionName)(args=self.functionArguments)
+                resultsDictionary[self.result_output_key] = functionDictionary.get(self.functionName)(self.functionArguments)
             else:
                 resultsDictionary[self.result_output_key] = functionDictionary.get(self.functionName)(self.functionArguments, self.functionKeyWordArguments)
         except Exception as ex:
