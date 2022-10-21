352c95c3bf453354adafa3ebd7cddb53b3e0d3e9
trrmann <tracy.mann1@retailbusinessservices.com>
2022-10-21 15:56:27 -0400

    deployed

--- tests/test_testCases.py
+++ tests/test_testCases.py
@@ -24,7 +24,7 @@ print(Test( "unit/functional/regression test", {
             "TestCase",
             expectedOutputValueOf=TestCase("","",""),
             functionArguments = {
-                "arguments" : ["","",""]
+                "arguments" : ("","","")
             }
         ),
         TestCase(
